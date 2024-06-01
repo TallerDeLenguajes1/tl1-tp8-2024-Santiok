2c568ee493f673f2546bc5d9559fa8433ca7d0fa
“Santiago” <santimedinaok.10@gmail.com>
2024-06-01 19:11:13 -0300

    Punto 1 realizado

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿using EspacioTarea;
 
-//FUNCION PARA MOVER TAREA.
-void moverTarea(List<Tarea> pendientes);
-
-
-
     int N, opcion;
     Console.WriteLine("\nIngrese la cantidad de tareas pendientes:\n");
     int.TryParse(Console.ReadLine(), out N);
@@ -16,15 +11,17 @@ void moverTarea(List<Tarea> pendientes);
     for (int i = 0; i < N; i++)
     {
         Random random = new Random();
-        int duracion = random.Next(10, 91), cont = 1;
+        int duracion = random.Next(10, 91);
         Console.WriteLine("\nIngrese una descripcion para la tarea:\n");
         string descripcion = Console.ReadLine();
 
-        Tarea tarea = new Tarea(cont, descripcion, duracion);
+        Tarea tarea = new Tarea(i+1, descripcion, duracion);
 
         tareasPendientes.Add(tarea);
-        cont++;
+   
     }
+
+    //Menu de opciones.
 
     do
     {
@@ -35,11 +32,44 @@ void moverTarea(List<Tarea> pendientes);
         switch (opcion)
         {
             case 1:
+                Tarea tareaAux = new Tarea();
+
                 Console.WriteLine("\nIngrese el ID de la tarea que desea mover:\n");
                 int.TryParse(Console.ReadLine(), out aux1);
+
+                tareaAux = moverTarea(tareasPendientes, aux1);
+
+                tareasRealizadas.Add(tareaAux);
+                tareasPendientes.Remove(tareaAux);
+
+                Console.WriteLine("\n--------------------Tareas Pendientes--------------------\n");
+                mostrarTarea(tareasPendientes);
+                Console.WriteLine("\n--------------------Tareas Realizadas--------------------\n");
+                mostrarTarea(tareasRealizadas);
+
             break;
 
             case 2:
+                Tarea tareaAux2 = new Tarea();
+                bool contiene;
+
+                Console.WriteLine("\nIngrese una palabra clave:\n");
+                string buscar = Console.ReadLine();
+
+                foreach (var tarea in tareasPendientes)
+                {
+                    contiene = tarea.descripcionTarea.Contains(buscar);
+
+                    if (contiene)
+                    {
+                        tareaAux2 = tarea;
+                    }
+                }
+
+                Console.WriteLine("\nEl ID de la tarea es: " + tareaAux2.idTarea + "\n");
+                Console.WriteLine("\nDescripcion de la tarea: " + tareaAux2.descripcionTarea + "\n");
+                Console.WriteLine("\nLa duracion de la tarea es: " + tareaAux2.duracionTarea + "\n");
+
             break;
 
             case 3:
@@ -54,4 +84,27 @@ void moverTarea(List<Tarea> pendientes);
     } while (opcion != 3);
 
 
+//FUNCION PARA MOVER TAREA.
+Tarea moverTarea(List<Tarea> pendientes, int id)
+{
+    Tarea tareaAux = new Tarea();
+    foreach (var tarea in pendientes)
+    {
+        if (tarea.idTarea == id)
+        {
+            tareaAux = tarea;
+        }
+    }
+    return tareaAux;
+}
 
+//FUNCION PARA MOSTRAR LAS TAREAS.
+void mostrarTarea(List<Tarea> misTarea)
+{
+    foreach (var tarea in misTarea)
+    {
+        Console.WriteLine("\nEl ID de la tarea es: " + tarea.idTarea + "\n");
+        Console.WriteLine("\nDescripcion de la tarea: " + tarea.descripcionTarea + "\n");
+        Console.WriteLine("\nLa duracion de la tarea es: " + tarea.duracionTarea + "\n");
+    }
+}
--- Tareas.cs
+++ Tareas.cs
@@ -5,10 +5,31 @@ public class Tarea
     private string descripcion;
     private int duracion;
 
+    public Tarea()
+    {
+        
+    }
+
+    //Metodo constructor.
     public Tarea(int miId, string miDescripcion, int auxDuracion)
     {
         this.tareaID = miId;
         this.descripcion = miDescripcion;
         this.duracion = auxDuracion;
     }
+
+    //Metodo para acceder a los campos de la clase.
+    public int idTarea
+    {
+        get { return tareaID; }
+    }
+    public string descripcionTarea
+    {
+        get { return descripcion ;}
+    }
+    public int duracionTarea
+    {
+        get { return duracion; }
+    }
+
 }
